4bc57f6f3a96fb258bf469f1f7bebebe36f77b5e
Saija Saarenpää <saija.saarenpaa@vincit.fi>
2017-03-04 11:16:43 +0900

    Add project

---
+++ LaplandApplication/LaplandApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaplandApplication
+//
+//  Created by Saija Saarenpää on 04/03/2017.
+//  Copyright © 2017 try! Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
