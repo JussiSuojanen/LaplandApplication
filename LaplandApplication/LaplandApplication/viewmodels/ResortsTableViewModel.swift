612ff250d941394a67e321baf915bde7261508fd
Saija Saarenpää <saija.saarenpaa@vincit.fi>
2017-03-04 17:03:26 +0900

    Add SantaResort

--- LaplandApplication/LaplandApplication/viewmodels/ResortsTableViewModel.swift
+++ LaplandApplication/LaplandApplication/viewmodels/ResortsTableViewModel.swift
@@ -9,7 +9,7 @@ class ResortsTableViewModel {
             switch result {
             case .success(let resorts):
                 self?.resorts.value = resorts.flatMap { SkiResortViewModel($0)}
-                let resort = Resort(id: 1,
+                let moominResort = Resort(id: 1,
                                     type: 2,
                                     name: "Muumimaailma",
                                     height: 0,
@@ -19,8 +19,20 @@ class ResortsTableViewModel {
                                     numberOfLiftsOpen: 0,
                                     crossCountrySki: 0,
                                     defaultImage: "http://swiftyjimmy.com/skiresorts/muumimaailma/muumi1.jpg")
-                let resortViewModel = MoominResortViewModel(resort)
-                self?.resorts.value.append(resortViewModel);
+                let moominResortViewModel = MoominResortViewModel(moominResort)
+                self?.resorts.value.append(moominResortViewModel);
+                let santaResort = Resort(id: 1,
+                                          type: 2,
+                                          name: "SantaPark",
+                                          height: 0,
+                                          numberOfSlopes: 0,
+                                          numberOfSlopesOpen: 0,
+                                          numberOfLifts: 0,
+                                          numberOfLiftsOpen: 0,
+                                          crossCountrySki: 0,
+                                          defaultImage: "http://swiftyjimmy.com/skiresorts/muumimaailma/muumi1.jpg")
+                let santaResortViewModel = SantaResortViewModel(santaResort)
+                self?.resorts.value.append(santaResortViewModel);
             case .failure(_):
                 break
             }
