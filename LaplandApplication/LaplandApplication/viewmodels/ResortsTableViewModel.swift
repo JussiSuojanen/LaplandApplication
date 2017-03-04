8c7ddf6d8eebea969886f11081b4aaa6c33bc036
Saija Saarenpää <saija.saarenpaa@vincit.fi>
2017-03-04 17:09:41 +0900

    Add santapark image

--- LaplandApplication/LaplandApplication/viewmodels/ResortsTableViewModel.swift
+++ LaplandApplication/LaplandApplication/viewmodels/ResortsTableViewModel.swift
@@ -9,34 +9,38 @@ class ResortsTableViewModel {
             switch result {
             case .success(let resorts):
                 self?.resorts.value = resorts.flatMap { SkiResortViewModel($0)}
-                let moominResort = Resort(id: 1,
-                                    type: 2,
-                                    name: "Muumimaailma",
-                                    height: 0,
-                                    numberOfSlopes: 0,
-                                    numberOfSlopesOpen: 0,
-                                    numberOfLifts: 0,
-                                    numberOfLiftsOpen: 0,
-                                    crossCountrySki: 0,
-                                    defaultImage: "http://swiftyjimmy.com/skiresorts/muumimaailma/muumi1.jpg")
-                let moominResortViewModel = MoominResortViewModel(moominResort)
-                self?.resorts.value.append(moominResortViewModel);
-                let santaResort = Resort(id: 1,
-                                          type: 2,
-                                          name: "SantaPark",
-                                          height: 0,
-                                          numberOfSlopes: 0,
-                                          numberOfSlopesOpen: 0,
-                                          numberOfLifts: 0,
-                                          numberOfLiftsOpen: 0,
-                                          crossCountrySki: 0,
-                                          defaultImage: "http://swiftyjimmy.com/skiresorts/muumimaailma/muumi1.jpg")
-                let santaResortViewModel = SantaResortViewModel(santaResort)
-                self?.resorts.value.append(santaResortViewModel);
+                self?.insertExtraResorts()
             case .failure(_):
+                self?.insertExtraResorts()
                 break
             }
         }
+    }
 
+    func insertExtraResorts() {
+        let moominResort = Resort(id: 1,
+                                  type: 2,
+                                  name: "Muumimaailma",
+                                  height: 0,
+                                  numberOfSlopes: 0,
+                                  numberOfSlopesOpen: 0,
+                                  numberOfLifts: 0,
+                                  numberOfLiftsOpen: 0,
+                                  crossCountrySki: 0,
+                                  defaultImage: "http://swiftyjimmy.com/skiresorts/muumimaailma/muumi1.jpg")
+        let moominResortViewModel = MoominResortViewModel(moominResort)
+        self.resorts.value.append(moominResortViewModel);
+        let santaResort = Resort(id: 2,
+                                 type: 3,
+                                 name: "SantaPark",
+                                 height: 0,
+                                 numberOfSlopes: 0,
+                                 numberOfSlopesOpen: 0,
+                                 numberOfLifts: 0,
+                                 numberOfLiftsOpen: 0,
+                                 crossCountrySki: 0,
+                                 defaultImage: "http://swiftyjimmy.com/skiresorts/korvatunturi/santa1.jpg")
+        let santaResortViewModel = SantaResortViewModel(santaResort)
+        self.resorts.value.append(santaResortViewModel);
     }
 }
